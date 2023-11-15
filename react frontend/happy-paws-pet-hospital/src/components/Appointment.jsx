21ae7d75bd285efa06b2f3ee75d867f8b15ed635
Emilija Vaisvalavičiūtė <e.vaisva@gmail.com>
2023-11-15 21:05:14 +0200

    Update appointments

---
+++ react frontend/happy-paws-pet-hospital/src/components/Appointment.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+
+const Appointment = ({
+  time,
+  status = 'Scheduled',
+  petName,
+  doctorName,
+  procedures
+}) => {
+  useEffect(() => {
+    if (status === 'Done') {
+      setStatusStyle('success')
+    } else if (status === 'Cancelled') {
+      setStatusStyle('danger')
+    }
+  }, [])
+
+  const [statusStyle, setStatusStyle] = useState('primary')
+
+  return (
+    <div className='col-12 mt-3'>
+      <div className='card'>
+        <div className='card-body'>
+          <h5 className='card-title'>{time}</h5>
+          <p className={`badge bg-${statusStyle}`}>{status}</p>
+          <p className='card-text'>Pet: {petName}</p>
+          <p className='card-text'>Doctor: {doctorName}</p>
+          <p className='card-text'>Procedures: {procedures}</p>
+          <div className='btn-toolbar' role='toolbar'>
+            <button type='button' className='btn btn-secondary mr-2'>
+              View
+            </button>
+            <button type='button' className='btn btn-secondary mr-2'>
+              Edit
+            </button>
+            <button type='button' className='btn btn-danger mr-2'>
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Appointment
--- react frontend/happy-paws-pet-hospital/src/pages/Appointments.jsx
+++ react frontend/happy-paws-pet-hospital/src/pages/Appointments.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React from 'react'
+import Appointment from '../components/Appointment'
 
 const Appointments = () => {
-  return <div>Appointments</div>;
-};
+  return (
+    <>
+      <div className='container d-flex align-items-center'>
+        <div className='row mt-3'>
+          <Appointment
+            time={'2023-11-25 13:00-14:00'}
+            status={'Scheduled'}
+            petName={'Nancy'}
+            doctorName={'Sarah Smith'}
+            procedures={
+              'Procedures: Feline infectious peritonitis (FIP) vaccination'
+            }
+          />
+          <Appointment
+            time={'2023-10-30 10:30-11:30'}
+            status={'Scheduled'}
+            petName={'Oreo'}
+            doctorName={'Lily Anderson'}
+            procedures={'Dental extraction'}
+          />
+          <Appointment
+            time={'2022-09-13 12:30-13:30'}
+            status={'Cancelled'}
+            petName={'Dave'}
+            doctorName={'Liam Herwig'}
+            procedures={'Wellness exam'}
+          />
+          <Appointment
+            time={'2022-09-13 12:30-13:30'}
+            status={'Cancelled'}
+            petName={'Dave'}
+            doctorName={'Liam Herwig'}
+            procedures={'Wellness exam'}
+          />
+          <Appointment
+            time={'2022-09-13 12:30-13:30'}
+            status={'Done'}
+            petName={'Smudge'}
+            doctorName={'Liam Herwig'}
+            procedures={'Wellness exam, Nail trim'}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
 
-export default Appointments;
+export default Appointments
