62ec34b7cf758cd100e8791a639dfe1bd7a7ac60
Emilija Vaisvalavičiūtė <e.vaisva@gmail.com>
2023-12-05 23:47:34 +0200

    dogtor

--- react frontend/happy-paws-pet-hospital/src/pages/Doctors.jsx
+++ react frontend/happy-paws-pet-hospital/src/pages/Doctors.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react'
 import CirclePicture from '../components/CirclePicture'
-import { useDispatch, useSelector } from 'react-redux';
-import { getDoctors } from '../reducers/user';
+import { useDispatch, useSelector } from 'react-redux'
+import { getDoctors } from '../reducers/user'
 
 const Doctors = () => {
-  const dispatch = useDispatch();
-  const {doctors} = useSelector(state => state.user)
+  const dispatch = useDispatch()
+  const { doctors } = useSelector(state => state.user)
 
   useEffect(() => {
     dispatch(getDoctors())
@@ -19,6 +19,8 @@ const Doctors = () => {
             key={doctor.id}
             title={doctor.name + ' ' + doctor.surname}
             imageSource={doctor.photo}
+            phoneNumber={doctor.phoneNumber}
+            email={doctor.email}
           />
         ))}
       </div>
